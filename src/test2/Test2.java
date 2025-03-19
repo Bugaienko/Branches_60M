d2d48421525257723ca57201fd9593ae1d162b73
SergeyB <sergiibugaienko@gmail.com>
2025-03-19 11:19:15 +0100

    test2 add

---
+++ src/test2/Test2.java
@@ -0,0 +1,14 @@
+package test2;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 19.03.2025
+ */
+
+public class Test2 {
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
