50e941bf37a56d8f482ca3e9723a3da4807e54c6
SergeyB <sergiibugaienko@gmail.com>
2025-03-19 11:52:24 +0100

    task1 done

---
+++ src/anonymous/MyClass.java
@@ -0,0 +1,13 @@
+package anonymous;
+
+/**
+ * @author Sergey Bugaenko
+ * {@code @date} 19.03.2025
+ */
+
+public class MyClass {
+    @Override
+    public String toString() {
+        return "Sergey";
+    }
+}
