87211f280f55cfa13453c588cb1669bd50fd77ee
Panfilovs <maksims_panfilovs@yahoo.com>
2025-03-19 12:16:10 +0100

    task1 update

--- src/panfilovs/Maksim.java
+++ src/panfilovs/Maksim.java
@@ -1,44 +1,9 @@
 package panfilovs;
 
-import java.util.Objects;
-
-public class Panv {
-
-    private String name;
-    private int age;
-
-    public Panv(String hoby, int age) {
-        this.name = hoby;
-        this.age = age;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (!(o instanceof Panv)) return false;
-        Panv panv = (Panv) o;
-        return age == panv.age && Objects.equals(name, panv.name);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(name, age);
-    }
+public class Maksim {
 
     @Override
     public String toString() {
-        return "Panv{" +
-                "hoby='" + name + '\'' +
-                ", age=" + age +
-                '}';
-    }
-
-    public String getName() {
-        return name;
-    }
-
-
-
-    public int getAge() {
-        return age;
+        return "Максим Панфилов";
     }
 }
