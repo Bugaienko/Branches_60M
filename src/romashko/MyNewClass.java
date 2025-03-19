49d706f6a372a7a9f1d3b1590efc662032ccda42
Romyleads <romyleads@gmail.com>
2025-03-19 13:06:36 +0100

    created new class

---
+++ src/romashko/MyNewClass.java
@@ -0,0 +1,10 @@
+package romashko;
+
+/*** Author: Roman Romashko Date: 19.03.2025 ***/
+
+public class MyNewClass {
+    @Override
+    public String toString() {
+        return "Roman";
+    }
+}
