727e49b859f922cbdd82699ff11595d59cd2b50c
Oleksandr <70403509@gmx.de>
2025-03-19 11:54:02 +0100

    class MyName added

---
+++ src/Oborskyi/MyName.java
@@ -0,0 +1,12 @@
+package Oborskyi;
+
+public class MyName {
+    @Override
+    public String toString() {
+        return "MyName: Oleksandr";
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new MyName());
+    }
+}
