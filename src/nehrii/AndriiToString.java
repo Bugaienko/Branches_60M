a429632a8d0122949e3319dd3776fdbe256f6a9c
Andrii-Nehrii <negriyandriy@gmail.com>
2025-03-19 11:53:59 +0100

    add my name toString

---
+++ src/nehrii/AndriiToString.java
@@ -0,0 +1,14 @@
+package nehrii;
+
+
+
+public class AndriiToString {
+    private  String name = "Andrii";
+
+    @Override
+    public String toString() {
+        return "AndriiToString{" +
+                "name='" + name + '\'' +
+                '}';
+    }
+}
