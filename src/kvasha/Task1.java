719337831061963585b0c475b98ca3bd6f47be22
VitaliiK <whitetech0047@gmail.com>
2025-03-19 12:16:03 +0100

    update toString()

--- src/kvasha/Task1.java
+++ src/kvasha/Task1.java
@@ -1,24 +1,10 @@
 package kvasha;
 
 public class Task1 {
-    private String name;
-
-    public Task1() {
-        this.name = "Виталий";
-    }
 
     @Override
     public String toString() {
-        return "Task1{" +
-                "name='" + name + '\'' +
-                '}';
-    }
-
-    public static void main(String[] args) {
-
-        Task1 defaultTask = new Task1();
-        System.out.println(defaultTask);
-
+        return "Виталий";
     }
 }
 
